8dde902be30dfff2f2b5d2d24cbd22e7b8751847
Viktoria Rosovska <torimeister@gmail.com>
2024-10-13 15:21:11 +0300

    update

--- src/index.js
+++ src/index.js
@@ -7,26 +7,14 @@ document.body.className = classes.body;
 console.log("Hello world!");
 
 function openMobileMenu() {
-  const toggleButton = document.querySelector(".menu-toggle");
   const menu = document.querySelector(".menu-mobile");
-
-  // Добавляем событие клика на кнопку гамбургер-меню
-  toggleButton.addEventListener("click", () => {
-    // Переключаем класс "show" для отображения/скрытия меню
-    menu.classList.add("show");
-
-    // Переключаем класс "rotate" для вращения иконки гамбургер-меню
-    // toggleButton.classList.toggle("rotate");
-  });
+  menu.classList.add("show");
 }
 
 function onCloseMobileMenu() {
-  const closeMobMenuBtn = document.querySelector(".menu-mobile__btn");
-  const menuMobile = document.querySelector(".menu-mobile");
-  closeMobMenuBtn.addEventListener("click", () => {
-    menuMobile.classList.remove("show");
-  });
+  const menu = document.querySelector(".menu-mobile");
+  menu.classList.remove("show");
 }
 
-window.openMobileMenu = openMobileMenu;
-window.onCloseMobileMenu = onCloseMobileMenu;
+document.querySelector(".menu-toggle").addEventListener("click", openMobileMenu);
+document.querySelector(".menu-mobile__btn").addEventListener("click", onCloseMobileMenu);
