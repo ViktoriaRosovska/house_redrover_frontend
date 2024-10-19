a92fb805f9d2ca232a42dc14786a6308d1dcb2bc
Viktoria Rosovska <torimeister@gmail.com>
2024-10-19 03:05:52 +0300

    add second json

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import classes from "./styles/styles.scss";
-
+let select = 1;
 const pictures = require("./data/pictures.json");
 console.log(pictures);
 
@@ -16,5 +16,24 @@ function onCloseMobileMenu() {
   menu.classList.remove("show");
 }
 
+function addActiveAndLoadJson(country) {
+  const countryName = document.querySelector(`${country}`);
+
+  const allBtns = document.querySelectorAll(".round-btn");
+  console.log(countryName);
+  console.log(allBtns);
+  for (el of allBtns) {
+    console.log(el);
+    el.classList.remove("active");
+  }
+  countryName.classList.add("active");
+  select = 2;
+  console.log(select);
+}
+
 document.querySelector(".menu-toggle").addEventListener("click", openMobileMenu);
 document.querySelector(".menu-mobile__btn").addEventListener("click", onCloseMobileMenu);
+
+document.querySelector(".germany").addEventListener("click", () => addActiveAndLoadJson(".germany"));
+document.querySelector(".england").addEventListener("click", () => addActiveAndLoadJson(".england"));
+document.querySelector(".france").addEventListener("click", () => addActiveAndLoadJson(".france"));
